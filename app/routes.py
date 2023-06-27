a87ecfa730e1f03af991ed76f93040a0fdf908f2
Nelson <nelsonds419@gmail.com>
2023-06-27 16:48:36 -0700

    reverted basic auth chages

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,9 @@
 import base64
-import hashlib
 import secrets
 
 from fastapi import (APIRouter, Depends, HTTPException, Request, Security,
                      responses, status)
-from fastapi.security import (HTTPAuthorizationCredentials, HTTPBasic,
-                              HTTPBasicCredentials, HTTPBearer, HTTPDigest)
+from fastapi.security import HTTPAuthorizationCredentials, HTTPDigest
 from starlette.datastructures import FormData
 from starlette.types import Message
 from twilio.request_validator import RequestValidator
@@ -45,45 +43,16 @@ class Routes():
             twiml_empty_response = "<Response/>"
             return responses.PlainTextResponse(content=twiml_empty_response, media_type="text/xml")
 
-    def _authorize(self, credentials: HTTPBasicCredentials = Security(HTTPBasic(auto_error=False))):
-        logger.info(f'credentialssss: {credentials}')
-
-        # incoming_token = credentials.credentials if credentials is not None else ''
-
-        incoming_username = credentials.username
-        incoming_password = credentials.password
-
-        incoming_token = base64.standard_b64encode(bytes(f"{incoming_username}:{incoming_password}", encoding='UTF-8')).decode('UTF-8')
-
-        expected_username = self.settings.secrets_app.app_username
-        expected_password = self.settings.secrets_app.app_password
-        expected_token = base64.standard_b64encode(bytes(f"{expected_username}:{expected_password}", encoding='UTF-8')).decode('UTF-8')
-
-        correct_token = secrets.compare_digest(incoming_token, expected_token)
-
-        logger.info(f'incoming_token: {incoming_token}')
-        logger.info(f'expected_token: {expected_token}')
-
-        if not correct_token:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect token", headers={'WWW-Authenticate': 'Basic'})
-
-    def _authorize_digest(self, credentials: HTTPAuthorizationCredentials = Security(HTTPDigest(auto_error=False))) -> None:
+    def _authorize(self, credentials: HTTPAuthorizationCredentials = Security(HTTPDigest(auto_error=False))):
         incoming_token = credentials.credentials if credentials is not None else ''
         expected_username = self.settings.secrets_app.app_username
         expected_password = self.settings.secrets_app.app_password
-        realm = 'TestRealm'
 
-        expected_token = hashlib.md5(f'{expected_username}:{realm}:{expected_password}'.encode('utf-8')).hexdigest()
-        logger.info(f'expected_token: {expected_token}')
-        correct_token = secrets.compare_digest(incoming_token, expected_token)
+        expected_token = base64.standard_b64encode(bytes(f"{expected_username}:{expected_password}", encoding='UTF-8'))
+        correct_token = secrets.compare_digest(bytes(incoming_token, encoding='UTF-8'), expected_token)
 
         if not correct_token:
-            challenge_params = f'Digest realm="{realm}"'
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="Incorrect digest token",
-                headers={"WWW-Authenticate": challenge_params}
-            )
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect token", headers={'WWW-Authenticate': 'Basic'})
 
     def validate_twilio_signature(self, request: Request, form: FormData):
         """
