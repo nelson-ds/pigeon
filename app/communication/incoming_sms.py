0f7feb4891c16d7c7bd35bf92f1763aab17ded71
Nelson <nelsonds419@gmail.com>
2023-06-28 15:55:48 -0700

    minor refactoring

--- app/communication/incoming_sms.py
+++ app/communication/incoming_sms.py
@@ -6,18 +6,10 @@ from utils.generic import logger
 
 class IncomingSms:
     def __init__(self, sms_from: str, sms_body: str, mongodb_dao: MongodbDao):
-        self.sms_from = sms_from,
-        self.sms_body = sms_body,
+        self.sms_from = sms_from
+        self.sms_body = sms_body
         self.mongodb_dao: MongodbDao = mongodb_dao
-        self.user: UsersDto = None
-
-        try:
-            self.user = mongodb_dao.get_user_by_phone_number(self.sms_from)
-        except MongoDbUserNotFoundException as e:
-            logger.info(f'User does not exist in DB; adding the user to the DB..')
-            self.user = self._add_new_user_to_db()
-        except Exception as e:
-            logger.error(f'Error while fetching user from DB: {str(e)}')
+        self.user: UsersDto = self._get_user_from_db()
 
     def get_response(self) -> str:
         twiml_success_response = """
@@ -34,12 +26,27 @@ class IncomingSms:
 
         return response
 
-    def _add_new_user_to_db(self):
-        user_dto: UsersDto = UsersDto('', self.sms_from)
+    def _get_user_from_db(self) -> UsersDto:
+        user = None
+        try:
+            user = self.mongodb_dao.get_user_by_phone_number(self.sms_from)
+            logger.info(f'User with phone number {self.sms_from} fetched from DB')
+        except MongoDbUserNotFoundException as e:
+            logger.info(f'User does not exist in DB; adding the user to the DB..')
+            user = self._add_new_user_to_db()
+        except Exception as e:
+            logger.error(f'Error while fetching user from DB: {str(e)}')
+        finally:
+            return user
+
+    def _add_new_user_to_db(self) -> UsersDto:
+        user_dto = UsersDto('', self.sms_from)
         try:
             inserted_id = self.mongodb_dao.insert_user(user_dto)
             logger.info(f"New user with phone number {self.sms_from} inserted in DB with ID: {inserted_id}")
             return user_dto
         except Exception as e:
             logger.error(f'Error while inserting new user in DB: {str(e)}')
-            return None
+            user_dto = None
+        finally:
+            return user_dto
