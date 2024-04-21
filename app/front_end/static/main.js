fe08a4b5e7b09180327ae43260a2cd4b90b6f9d4
Nelson <nelsonds419@gmail.com>
2024-04-21 13:38:12 -0700

    Updated web page

--- app/front_end/static/main.js
+++ app/front_end/static/main.js
@@ -7,13 +7,13 @@ document.addEventListener("DOMContentLoaded", function() {
             numberContainer.classList.remove('visible');
             numberContainer.style.visibility = 'hidden';
             numberContainer.style.opacity = '0';
-            revealButton.textContent = "Reveal Number"; // Change button text to "Reveal Number"
+            revealButton.textContent = "Reveal Number";
         } else {
             numberContainer.classList.add('visible');
             numberContainer.style.visibility = 'visible';
             numberContainer.style.opacity = '1';
-            numberContainer.textContent = "Not yet live - check back later!"; // Use your actual service number here
-            revealButton.textContent = "Hide Number"; // Change button text to "Hide Number"
+            numberContainer.textContent = "Not yet live - check back later!";
+            revealButton.textContent = "Hide Number";
         }
     });
 });
