92b875a00afe0d87a36929230933189a9af8b34b
Nelson <nelsonds419@gmail.com>
2024-04-21 11:56:52 -0700

    Update web-page

--- app/front_end/static/main.js
+++ app/front_end/static/main.js
@@ -1,12 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var dynamicContent = document.getElementById("pet-action");
-    var updateButton = document.getElementById("pet-button");
-    var clickCount = 0;
+    var revealButton = document.getElementById("reveal-button");
+    var numberContainer = document.getElementById("number-container");
 
-    updateButton.addEventListener("click", function() {
-        clickCount++;
-        var newParagraph = document.createElement("p");
-        newParagraph.textContent = ' coo'.repeat(clickCount);
-        dynamicContent.appendChild(newParagraph);
+    revealButton.addEventListener("click", function() {
+        if (numberContainer.classList.contains('visible')) {
+            numberContainer.classList.remove('visible');
+            numberContainer.style.visibility = 'hidden';
+            numberContainer.style.opacity = '0';
+            revealButton.textContent = "Reveal Number"; // Change button text to "Reveal Number"
+        } else {
+            numberContainer.classList.add('visible');
+            numberContainer.style.visibility = 'visible';
+            numberContainer.style.opacity = '1';
+            numberContainer.textContent = "Not yet live - check back later!"; // Use your actual service number here
+            revealButton.textContent = "Hide Number"; // Change button text to "Hide Number"
+        }
     });
 });
